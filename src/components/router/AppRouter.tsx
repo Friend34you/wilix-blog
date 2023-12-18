23456c031ceb4171e1af7ef53038d2495244fe64
blakuto <vovan23doni@gmail.com>
2023-12-18 10:34:11 +0300

    header: added condition

--- src/components/router/AppRouter.tsx
+++ src/components/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import {Outlet, RouterProvider, createBrowserRouter, Navigate} from "react-router-dom";
 import {privateRoutes, publicRoutes, Routes} from "./routes.tsx";
 import PrivateRoute from "./privateRoute.tsx";
+import AppHeader from "../AppHeader.tsx";
 
 //TODO: вместо рутовой заглушки добавить полноценный компонент
 //TODO: добавить NotFound component
@@ -9,7 +10,7 @@ const router = createBrowserRouter([
     path: "/",
     element: (
       <>
-        <h1>Header</h1>
+        <AppHeader />
         <div>Wrapper <Outlet /></div>
         <h1>Footer</h1>
         <Navigate to={Routes.ARTICLES} />
