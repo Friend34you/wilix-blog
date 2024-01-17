bdac0691c33de324e86b32e25bd8fe13e6f23bd8
blakuto <vovan23doni@gmail.com>
2024-01-17 17:21:09 +0300

    storybook: added msw

---
+++ src/stories/TagsCloud.stories.tsx
@@ -0,0 +1,29 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import TagsCloud from "../components/TagsCloud.tsx";
+import {rest} from "msw";
+
+const meta: Meta<typeof TagsCloud> = {
+  title: "TagsCloud",
+  component: TagsCloud,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TagsCloud>;
+
+export const Default: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get('/tags', (_, res, ctx) => {
+          return res(
+            ctx.json({
+              tags: ["tag1", "msw tag"]
+            })
+          );
+        }),
+      ]
+    },
+  }
+};
