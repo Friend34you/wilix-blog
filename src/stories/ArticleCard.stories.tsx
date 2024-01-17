b707e55b9e364c7ab243ed60d1c348bd43cd45a3
blakuto <vovan23doni@gmail.com>
2024-01-17 16:30:16 +0300

    storybook: created first story

--- src/stories/ArticleCard.stories.tsx
+++ src/stories/ArticleCard.stories.tsx
@@ -1,9 +1,12 @@
 import ArticleCard from "../components/ArticleCard.tsx";
 import type {Meta, StoryObj} from "@storybook/react";
+import {withRouter} from "storybook-addon-react-router-v6";
 
 const meta: Meta<typeof ArticleCard> = {
   title: "ArticleCard",
   component: ArticleCard,
+  tags: ['autodocs'],
+  decorators: [withRouter]
 };
 
 export default meta;
@@ -20,11 +23,10 @@ export const Default: Story = {
     tagList: ["tag1", "tag2"],
     title: "Test title",
     description: "Test description",
-    createdAt: "23.03.2023",
-    updatedAt: "23.03.2023",
+    createdAt: "2024-01-11T10:10:54.059Z",
+    updatedAt: "2024-01-11T10:10:54.059Z",
     favorited: true,
     favoritesCount: 2,
     slug: "Test-title-slug",
-    // onFavoriteClick: () => console.log("click"),
   }
 };
